82b6aa13e2b0b5e875ba1330c12a1ce7b9538a20
Daniel A <DanielAAad01@gmail.com>
2024-01-25 15:53:23 -0800

    Added fonts and colors to tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "white": "hsl(0, 0%, 100%)",
+        "LightPink": "hsl(275, 100%, 97%)",
+        "GrayishPurple": "hsl(292, 16%, 49%)",
+        "DarkPurple": "hsl(292, 42%, 14%)"
       },
+      fontFamily: {
+        'sans': ["WorkSans", "sans-serif"]
+      }
     },
   },
   plugins: [],
