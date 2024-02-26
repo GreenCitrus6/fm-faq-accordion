2d2a5d840f460e67993a86f430e30c3d16f33b2b
Daniel A <DanielAAad01@gmail.com>
2024-02-25 19:59:13 -0800

    Added responsive styling for desktop screen sizes

--- src/app/page.js
+++ src/app/page.js
@@ -24,58 +24,61 @@ function toggleAnswer(answer, icon) {
     document.getElementById(`${icon}-minus`).classList.add('hidden');
     document.getElementById(`${icon}-plus`).classList.remove('hidden');
   }
-
 }
+
 
 export default function Home() {
   return (
     <>
       {/* background elements */}
       <div className="bgElements w-full h-screen bg-LightPink absolute z-0">
-        <Image src={ bgPatternMobile } className="w-full z-0" />
+        <Image src={ bgPatternMobile } id="bg-image-mobile" className="w-full z-0 hidden" />
+        <Image src={ bgPatternDesktop } id="bg-image-desktop" className="w-full z-0 hidden" />
       </div>
 
       {/* FAQ accordion */}
-      <div className="accordionContainer h-screen w-full grid gap-1  grid-cols-1 grid-rows-1 justify-items-center items-center z-10 font-WorkSans">
-        <section className="faqAccordion col-span-1 max-h-[90%] w-full max-w-[90%] min-w-[285px] overflow-y-scroll overflow-x-hidden mx-10 px-5 pt-4 bg-white z-10 rounded-lg shadow-sm">
+      <div id="accordionContainer" className="h-screen w-full grid gap-1  grid-cols-1 grid-rows-1 justify-items-center items-center z-10 font-WorkSans">
+        <section id="accordionContents" className="faqAccordion col-span-1 max-h-[90%] w-full max-w-[90%] min-w-[285px]  overflow-x-hidden mx-10 px-5 pt-4 pb-5 bg-white z-10 rounded-lg shadow-sm">
           {/* FAQ HEADER */}
-          <h1 className="flex items-center w-full pt-2 pb-4"><Image src={ iconStar } width={25} /><span className="text-2xl font-extrabold pl-4 text-DarkPurple">FAQs</span></h1>
+          <h1 className="flex items-center w-full pt-2 pb-4"><Image src={ iconStar } className="w-[25px] min-[749px]:w-[40px]" /><span className="text-2xl font-extrabold pl-4 text-DarkPurple">FAQs</span></h1>
           {/* ACCORDION */}
           <section className="questionButton flex justify-between items-center" onClick={ () => toggleAnswer("0-ans", "icon-0") }>
-            <h2 className="font-bold text-DarkPurple w-3/4 pb-4 text-lg leading-5">What is Frontend Mentor, and how will it help me?</h2>
-            <Image id="icon-0-plus" src={ iconPlus } className="" alt="plus" />
-            <Image id="icon-0-minus" src= { iconMinus } className="hidden" alt="minus" />
+            <div className="flex items-center justify-between w-full">
+            <h2 className="font-bold text-DarkPurple w-3/4 pb-4 text-lg leading-5 hover:cursor-pointer hover:text-purple-700 ease-out duration-300 active:text-purple-400 align-middle">What is Frontend Mentor, and how will it help me?</h2>
+            <Image id="icon-0-plus" src={ iconPlus } className=" w-[35px] min-[749px]:w-[50px]" alt="plus" />
+            <Image id="icon-0-minus" src= { iconMinus } className="hidden w-[35px] min-[749px]:w-[50px]" alt="minus" />
+            </div>
           </section>
           
-          <section id="0-ans" className="questionAnswer h-0 transition-all delay-150 duration-300 overflow-hidden w-full">
+          <section id="0-ans" className="questionAnswer h-0 transition-all duration-300 overflow-hidden w-full">
             <p className="text-GrayishPurple">Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.</p>
           </section>
           <hr className="mt-2 pb-4 border-LightPink" />
           <section className="questionButton flex justify-between items-center" onClick={ () => toggleAnswer("1-ans", "icon-1") }>
-            <h2 className="font-bold text-DarkPurple w-3/4 pb-4 text-lg leading-5">Is Frontend Mentor free?</h2>
-            <Image id="icon-1-plus" src={ iconPlus } className="" alt="plus" />
-            <Image id="icon-1-minus" src= { iconMinus } className="hidden" alt="minus" />  
+            <h2 className="font-bold text-DarkPurple w-3/4 pb-4 text-lg leading-5 hover:cursor-pointer hover:text-purple-700 ease-out duration-300 active:text-purple-400 ">Is Frontend Mentor free?</h2>
+            <Image id="icon-1-plus" src={ iconPlus } className=" w-[35px] min-[749px]:w-[50px]" alt="plus" />
+            <Image id="icon-1-minus" src= { iconMinus } className="hidden w-[35px] min-[749px]:w-[50px]" alt="minus" />  
           </section>
 
-          <section id="1-ans" className="questionAnswer questionAnswer h-0 transition-all delay-150 duration-300 overflow-hidden w-full">
+          <section id="1-ans" className="questionAnswer questionAnswer h-0 transition-all duration-300 overflow-hidden w-full">
             <p className="text-GrayishPurple">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
           </section>
           <hr className="mt-2 pb-4 border-LightPink" />
           <section className="questionButton flex justify-between items-center" onClick={ () => toggleAnswer("2-ans", "icon-2") }>
-            <h2 className="font-bold text-DarkPurple w-3/4 pb-4 text-lg leading-5">Can I use Frontend Mentor projects in my portfolio?</h2>
-            <Image id="icon-2-plus" src={ iconPlus } className="" alt="plus" />
-            <Image id="icon-2-minus" src= { iconMinus } className="hidden" alt="minus" />
+            <h2 className="font-bold text-DarkPurple w-3/4 pb-4 text-lg leading-5 hover:cursor-pointer hover:text-purple-700 ease-out duration-300 active:text-purple-400 ">Can I use Frontend Mentor projects in my portfolio?</h2>
+            <Image id="icon-2-plus" src={ iconPlus } className=" w-[35px] min-[749px]:w-[50px]" alt="plus" />
+            <Image id="icon-2-minus" src= { iconMinus } className="hidden w-[35px] min-[749px]:w-[50px]" alt="minus" />
           </section>
-          <section id="2-ans" className="questionAnswer questionAnswer h-0 transition-all delay-150 duration-300 overflow-hidden w-full">
+          <section id="2-ans" className="questionAnswer questionAnswer h-0 transition-all duration-300 overflow-hidden w-full">
             <p className="text-GrayishPurple">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
           </section>
           <hr className="mt-2 pb-4 border-LightPink" />
           <section className="questionButton flex justify-between items-center" onClick={ () => toggleAnswer("3-ans", "icon-3") }>
-            <h2 className="font-bold text-DarkPurple w-3/4 pb-4 text-lg leading-5">How can I get help if I'm stuck on a challenge?</h2>
-            <Image id="icon-3-plus" src={ iconPlus } className="" alt="plus" />
-            <Image id="icon-3-minus" src= { iconMinus } className="hidden" alt="minus" />
+            <h2 className="font-bold text-DarkPurple w-3/4 pb-4 text-lg leading-5 hover:cursor-pointer hover:text-purple-700 ease-out duration-300 active:text-purple-400 ">How can I get help if I'm stuck on a challenge?</h2>
+            <Image id="icon-3-plus" src={ iconPlus } className=" w-[35px] min-[749px]:w-[50px]" alt="plus" />
+            <Image id="icon-3-minus" src= { iconMinus } className="hidden w-[35px] min-[749px]:w-[50px]" alt="minus" />
           </section>
-          <section id="3-ans" className="questionAnswer questionAnswer h-0 transition-all delay-150 duration-300 overflow-hidden w-full">
+          <section id="3-ans" className="questionAnswer questionAnswer h-0 transition-all duration-300 overflow-hidden w-full">
             <p className="text-GrayishPurple">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
           </section>
         </section>
